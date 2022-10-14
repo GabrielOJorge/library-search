44c89d574b9faa2c0ee27b6f743547ee8142e844
GabrielOJorge <goliver331@protonmail.com>
2022-10-14 17:45:07 -0300

    feat(): create Loader component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react';
 import Searchbar from './components/Searchbar';
 import Results from './components/Results';
+import Loader from './components/Loader';
 
 import './index.css'
 
 function App() {
-  const [results, setResults] = useState([]);
+  const [results, setResults] = useState(null);
   const [isSubmitted, changeSubmitValue] = useState(false);
   const [didResponseReturn, changeResponseReturnValue] = useState(false);
 
@@ -24,6 +25,7 @@ function App() {
   };
 
   const handleSubmit = async (e) => {
+    changeSubmitValue(true);
     e.preventDefault();
 
     const q = e.target[1].value;
@@ -36,23 +38,21 @@ function App() {
       console.error(err.message);
       changeResponseReturnValue(false);
     }
-
-    changeSubmitValue(true);
   };
 
   if (isSubmitted) {
     return (
-      <div className='container bg-neutral-900 h-screen flex flex-col items-center font-roboto-slab overflow-scroll'>
-        <main className='bg-white w-10/12 max-w-[800px] p-4 my-10 rounded'>
+      <div className='bg-neutral-900 w-full h-screen flex flex-col items-center font-roboto-slab overflow-scroll'>
+        <main className='bg-white flex flex-col w-10/12 max-w-[800px] p-4 my-10 rounded'>
           <Searchbar handleSubmit={handleSubmit}/>
-          
-          <Results results={results} didResponseReturn={didResponseReturn}/>
+
+          {results ? <Results results={results} didResponseReturn={didResponseReturn}/> : <Loader />}
         </main>
       </div>
     )
   } else {
     return (
-      <div className='container bg-neutral-900 h-screen flex flex-col items-center justify-center font-roboto-slab'>
+      <div className='bg-neutral-900 h-screen flex flex-col items-center justify-center font-roboto-slab'>
         <main className='bg-white w-11/12 max-w-[800px] p-4 rounded'>
           <Searchbar handleSubmit={handleSubmit}/>
         </main>
