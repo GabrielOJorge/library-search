607bfbfc08da5af8e8237bb75cd22e63454b11e2
GabrielOJorge <goliver331@protonmail.com>
2022-10-17 16:13:38 -0300

    refactor(): display Loader component in Results instead of App and display an error message in case of any error

--- src/components/Results/index.jsx
+++ src/components/Results/index.jsx
@@ -1,17 +1,27 @@
+import { useState } from "react";
 import Book from "../Book"
+import Loader from "../Loader";
 
-function Results({ results, didResponseReturn }) {
+function Results({ results, loading }) {
   const formatResults = (results) => {
     return results.map(result => <Book key={result.key} title={result.title} author={result.author_name[0]} date={result.first_publish_year} coverID={result.cover_edition_key}/>)
   };
 
-  if (results.length && didResponseReturn) {
+  if (loading) {
+    return <Loader />;
+  } else if (!results) {
     return (
-      <div className="flex flex-col gap-4">
-        {formatResults(results)}
+      <div>
+        <h1>Sorry, something went wrong. Please try again.</h1>
       </div>
     )
-  } else {
+  } else if (results.length) {
+    return (
+      <ul className="flex flex-col gap-4">
+        {formatResults(results)}
+      </ul>
+    )
+  } else if (!results.length) {
     return (
       <div>
         <h1>No results found.</h1>
