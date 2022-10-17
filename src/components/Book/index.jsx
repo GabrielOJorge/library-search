77052fd6a1e13d122d9263872b8310e0c8740411
GabrielOJorge <goliver331@protonmail.com>
2022-10-17 16:08:40 -0300

    refactor(): change div to li tag

--- src/components/Book/index.jsx
+++ src/components/Book/index.jsx
@@ -1,6 +1,6 @@
 function Book({ title, author, date, coverID }) {
   return (
-    <div className='bg-neutral-100 flex items-center flex-wrap gap-4 rounded p-4 shadow-neutral-200 shadow-md'>
+    <li className='bg-neutral-100 flex items-center flex-wrap gap-4 rounded p-4 shadow-neutral-200 shadow-md'>
       <img src={`https://covers.openlibrary.org/b/olid/${coverID}-M.jpg`} alt="Book cover" className="w-14 sm:w-16 md:w-20 rounded"/>
 
       <div>
@@ -8,7 +8,7 @@ function Book({ title, author, date, coverID }) {
         <h2 className='text-sm sm:text-base'>by {author}</h2>
         <h3 className='text-xs sm:text-sm text-neutral-500'>First published in {date}</h3>
       </div>
-    </div>
+    </li>
   )
 }
 
